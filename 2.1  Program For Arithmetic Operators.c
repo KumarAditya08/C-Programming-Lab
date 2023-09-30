783bb8f0ca0ea731dc59ecc87042fb707da596c5
Aditya Kumar <146500302+KumarAditya08@users.noreply.github.com>
2023-09-30 08:19:31 +0530

    Add files via upload

---
+++ 2.1  Program For Arithmetic Operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  float x,y,addition,subtraction,multiplication,division;
+  printf("Enter the first number : ");
+  scanf("%f",&x);
+  printf("Enter the second number : ");
+  scanf("%f",&y);
+  addition=x+y;
+  subtraction=x-y;
+  multiplication=x*y;
+  division=x/y;
+  printf("The addition of two numbers is %f : ",addition);
+  printf("\nThe subtraction of two numbers is %f : ",subtraction);
+  printf("\nThe multiplication of two numbers is %f : ",multiplication);
+  printf("\nThe division of two numbers is %f : ",division);
+ 
+  return 0;
+}
